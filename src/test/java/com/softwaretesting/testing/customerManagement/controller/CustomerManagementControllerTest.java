78b58be914a88230a41e168f9ae91cce36e8bcf5
Maik Wegener <maik.wegener@gwdg.de>
2023-06-06 11:33:11 +0200

    add MockMVC test for CustomerManagementController and improved test for CustomerRegistrationController

--- src/test/java/com/softwaretesting/testing/customerManagement/controller/CustomerManagementControllerTest.java
+++ src/test/java/com/softwaretesting/testing/customerManagement/controller/CustomerManagementControllerTest.java
@@ -1,8 +1,7 @@
-package com.softwaretesting.testing.customerRegistration.controller;
+package com.softwaretesting.testing.customerManagement.controller;
 
 import com.softwaretesting.testing.model.Customer;
 import net.datafaker.Faker;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +9,24 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class CustomerRegistrationControllerTest {
+class CustomerManagementControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    private final String endpoint_base = "/api/v1/customer-registration";
+    private final String endpoint_base = "/api/v1/customers";
 
     final Faker dataFaker = new Faker();
 
@@ -40,38 +44,36 @@ class CustomerRegistrationControllerTest {
                 "\"phoneNumber\": \"" + customer.getPhoneNumber() + "\"}";
     }
 
-
     @Test
     @DisplayName("Successfully add a new Customer using the customer-registration endpoint")
     void createCustomerTest() throws Exception {
-        final Customer customer = getSampleCustomer();
+        final String requestBody = getCustomerJson(getSampleCustomer());
 
         mockMvc.perform(post(endpoint_base)
-                        .content(getCustomerJson(customer))
+                        .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(getCustomerJson(customer)));
+                .andExpect(content().json(requestBody));
     }
 
 
     @Test
-    @Disabled("This test fails due to missing exception handling in the code")
     @DisplayName("Unsuccessfully try to add same Customer twice")
     void createCustomerTwiceTest() throws Exception {
-        final Customer customer = getSampleCustomer();
+        final String requestBody = getCustomerJson(getSampleCustomer());
 
         // first, add a valid customer
         mockMvc.perform(post(endpoint_base)
-                        .content(getCustomerJson(customer))
+                        .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(getCustomerJson(customer)));
+                .andExpect(content().json(requestBody));
 
         // then try to add the same customer again
         mockMvc.perform(post(endpoint_base)
-                        .content(getCustomerJson(customer))
+                        .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
@@ -99,12 +101,73 @@ class CustomerRegistrationControllerTest {
     }
 
     @Test
-    @DisplayName("Test not allowed GET-Method")
+    @DisplayName("Test GET with customer ID.")
+    void getCustomerByIdTest() throws Exception {
+        final Customer customer = getSampleCustomer();
+        final Pattern idRegex = Pattern.compile("\"id\":(\\d{1,5})");
+
+        final MvcResult result = mockMvc.perform(post(endpoint_base)
+                        .content(getCustomerJson(customer))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(getCustomerJson(customer)))
+                .andReturn();
+
+        final String response = result.getResponse().getContentAsString();
+        final Matcher idMatcher = idRegex.matcher(response);
+        if (idMatcher.find()) {
+            final String id = idMatcher.group(1);
+
+            mockMvc.perform(get(endpoint_base + "/{id}", id))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(content().json(getCustomerJson(customer)));
+        } else {
+            fail();
+        }
+    }
+
+    @Test
+    @DisplayName("Test GET without ID")
     void notAllowedGetTest() throws Exception {
         mockMvc.perform(get(endpoint_base))
                 .andExpect(status().isMethodNotAllowed());
     }
 
+    @Test
+    @DisplayName("Test GET with non-existent ID")
+    void getNonExistentCustomerTest() throws Exception {
+        // not sure if -1 is a good test value here,
+        // one could argue that negative values should cause a bad request (400) instead of not found (404)
+        mockMvc.perform(get(endpoint_base + "/{id}", "-1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Test DELETE with customer ID. Only works if createCustomerTest() was already executed.")
+    void deleteCustomerByIdTest() throws Exception {
+        final Customer customer = getSampleCustomer();
+        final Pattern idRegex = Pattern.compile("\"id\":(\\d{1,5})");
+
+        final MvcResult result = mockMvc.perform(post(endpoint_base)
+                        .content(getCustomerJson(customer))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(getCustomerJson(customer)))
+                .andReturn();
+
+        final String response = result.getResponse().getContentAsString();
+        final Matcher idMatcher = idRegex.matcher(response);
+        if (idMatcher.find()) {
+            final String id = idMatcher.group(1);
+            mockMvc.perform(delete(endpoint_base + "/{id}", id))
+                    .andExpect(status().isOk());
+        } else {
+            fail();
+        }
+    }
 
     @Test
     @DisplayName("Test not allowed DELETE-Method")
@@ -114,9 +177,20 @@ class CustomerRegistrationControllerTest {
     }
 
     @Test
+    @DisplayName("Test DELETE with non-existent ID")
+    void deleteNonExistentCustomerTest() throws Exception {
+        // not sure if -1 is a good test value here,
+        // one could argue that negative values should cause a bad request (400) instead of not found (404)
+        mockMvc.perform(delete(endpoint_base + "/{id}", "-1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
     @DisplayName("Test not found list GET-Method")
     void unavailableListMethodTest() throws Exception {
+        // TODO: check response body somehow
         mockMvc.perform(get(endpoint_base + "/list"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
